9a3e94326bf7c47f60ff58e98f4eca3b8b4f6223
Yousef ElKhawaga <yousefkhawaga@hotmail.com>
2021-01-25 04:19:56 +0200

    add test

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup (
   name = 'KhawagaNeuralNetwork',         # How you named your package folder (MyLib)
   packages = ['KhawagaNeuralNetwork'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='Apache License 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep Learning Framework that has multiple operations that could preprocess data, train and test the model with good accuracy.',   # Give a short description about your library
   author = 'Youssef khawaga',                   # Type in your name
   author_email = 'yousefkhawaga@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/YousefKhawaga/KhawagaNeuralNetwork',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/YousefKhawaga/KhawagaNeuralNetwork/archive/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/YousefKhawaga/KhawagaNeuralNetwork/archive/0.5.tar.gz',    # I explain this later on
   keywords = ['DeepLearning', 'Tool', 'Framework'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
