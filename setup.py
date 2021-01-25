9d6b9d70568adf1fa476707d819710ef1bee7718
Yousef ElKhawaga <yousefkhawaga@hotmail.com>
2021-01-25 03:03:03 +0200

    Package

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-
 from distutils.core import setup
 setup (
-  name = 'NeuralNetwork',         # How you named your package folder (MyLib)
-  packages = ['NeuralNetwork'],   # Chose the same as "name"
+  name = 'KhawagaNeuralNetwork',         # How you named your package folder (MyLib)
+  packages = ['KhawagaNeuralNetwork'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='Apache License 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep Learning Framework that has multiple operations that could preprocess data, train and test the model with good accuracy.',   # Give a short description about your library
   author = 'Youssef khawaga',                   # Type in your name
   author_email = 'yousefkhawaga@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/YousefKhawaga/NeuralNetwork',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/YousefKhawaga/NeuralNetwork/archive/0.1.tar.gz',    # I explain this later on
+  url = 'https://github.com/YousefKhawaga/KhawagaNeuralNetwork',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/YousefKhawaga/KhawagaNeuralNetwork/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['DeepLearning', 'Tool', 'Framework'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -28,4 +27,4 @@ setup (
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
-
+)
